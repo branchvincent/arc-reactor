e2d885a40882b2c4b737f6d66ca9011d0ad9e2ef
Mark Draelos <mark.draelos@duke.edu>
2016-12-26 19:13:05 -0500

    switched to Tornado for datababse HTTP interface

--- src/pensive/server.py
+++ src/pensive/server.py
@@ -12,7 +12,8 @@ logger = logging.getLogger(__name__)
 
 import json
 
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application
 
 from .core import Store
 
@@ -21,73 +22,52 @@ class StoreManager(object):
     def __init__(self):
         self._store = Store
 
-class PensiveHandler(BaseHTTPRequestHandler):
+class PensiveHandler(RequestHandler):
 
-    protocol_version = 'HTTP/1.1'
-    server_version = 'Pensive/1.0'
+    def get(self, path):
+        self.write({'path': path})
 
-    def do_GET(self):
-        self.send_json(self.path)
+class PensiveServer(Application):
+    '''
+    Tornado web application for database access over HTTP.
+    '''
+    def __init__(self, port, address=''):
+        super(PensiveServer, self).__init__()
+        self._port = port
+        self._address = address
 
-    def send_json(self, obj, code=200, headers=None):
-        if not headers:
-            headers = {}
-        # indicate JSON content type
-        headers['Content-Type'] = 'application/json; charset=utf-8'
-        # generate the response
-        self.send_response(json.dumps(
-            obj, indent=4, sort_keys=True, encoding='utf-8'), code, headers)
+        # install handlers for various URLs
+        self.add_handlers(r'.*', [(r'/(.*)', PensiveHandler)])
 
-    def send_response(self, data, code=200, headers=None):
-        if not headers:
-            headers = {}
+    def log_request(self, handler):
+        '''
+        Log the request and response information to module logger.
+        '''
+        # choose the severity level based on HTTP status codes
+        if handler.get_status() < 400:
+            log = logger.info
+        elif handler.get_status() < 500:
+            log = logger.warning
+        else:
+            log = logger.error
 
-        # set standard headers if not overridden
-        headers.setdefault('Server', self.version_string())
-        headers.setdefault('Date', self.date_time_string())
+        log('{} {} {} {} {} {:.2f}ms'.format(
+            handler.request.remote_ip,
+            handler.get_current_user() or '-',
+            handler.request.method,
+            handler.request.uri,
+            handler.get_status(),
+            1000 * handler.request.request_time())
+        )
 
-        if data:
-            # default to plaintext response
-            headers.setdefault('Content-Type', 'text/plain; charset=utf-8')
-            # generate content length if not set
-            headers.setdefault('Content-Length', len(data))
+    def run(self):
+        '''
+        Start servicing the Tornado event loop.
+        '''
+        # bind the socket
+        self.listen(self._port, self._address)
+        logger.info('Pensive started on {}:{}'.format(
+            self._address or '*', self._port))
 
-        # log the message early in case the response goes badly
-        self.log_request(code, headers.get('Content-Length', '-'))
-
-        response = []
-
-        # send the status line
-        message = self.responses.get(code, ('',))[0]
-        response.append('{} {} {}'.format(
-            self.protocol_version, code, message))
-
-        # send standard headers first
-        for k in ['Server', 'Date', 'Content-Type', 'Content-Length']:
-            if k in headers:
-                response.append('{}: {}'.format(k, headers.pop(k)))
-        # send the remaining headers
-        for k in sorted(headers.keys()):
-            response.append('{}: {}'.format(k, headers.pop(k)))
-
-        # add delimiting newline
-        response.append('')
-
-        # add payload if given
-        if data:
-            response.append(data)
-
-        # send the head and payload
-        self.wfile.write('\r\n'.join(response))
-
-    def log_message(self, format, *args):
-        logger.info(format % args)
-
-    def log_error(self, format, *args):
-        logger.error(format % args)
-
-class PensiveServer(HTTPServer):
-    def __init__(self, server_address):
-        HTTPServer.__init__(self, server_address, PensiveHandler)
-
-        self.manager = StoreManager()
+        # NOTE: this blocks until the event loop finishes
+        IOLoop.current().start()
