8d4c63328929d94535c804b9204743b5d67550da
Mark Draelos <mark.draelos@duke.edu>
2016-12-27 15:40:55 -0500

    implement basic HTTP interface for database

--- src/pensive/server.py
+++ src/pensive/server.py
@@ -2,30 +2,90 @@
 Web server interface for Pensive.
 
 The server uses the following URL structure.
+ * `/d/<path>`: access the default store
+ * `/i/<instance>/<path>`: access the named store `instance`
+ * `/s/<instance>`: control the named store `instance`
 
-
+When accessing a store, the HTTP verbs GET, POST, and DELETE are mapped to
+`get()`, `put()`, and `delete()`, respectively. The response to a GET is
+a JSON-encoded dictionary of the form `{"data": <value>}`. Similarly,
+the request body for a POST should be a JSON-encoded dictionary of the form
+`{"data": <value>}`.
 
 '''
 
 import logging
 logger = logging.getLogger(__name__)
 
-import json
-
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application
+from tornado.escape import json_decode
 
 from .core import Store
 
 class StoreManager(object):
+    '''
+    Container class for a dictionary of names to `Store`.
 
+    The default `Store` has key of `None`.
+    '''
     def __init__(self):
-        self._store = Store
+        self.stores = {
+            None: Store()
+        }
 
-class PensiveHandler(RequestHandler):
+class StoreHandler(RequestHandler):
+    '''
+    Handler for GET, POST, and DELETE for accessing the `Store`.
+    '''
 
-    def get(self, path):
-        self.write({'path': path})
+    def initialize(self):
+        '''
+        Initialize the handler with the applications `StoreManager`.
+        '''
+        self.manager = self.application.manager
+
+    def get(self, path, instance=None):
+        '''
+        Handle GET requests.
+        '''
+        try:
+            store = self.manager.stores[instance]
+        except KeyError:
+            logger.warn('unrecognized instance: {}'.format(instance))
+            self.send_error(400, reason='unrecognized instance')
+        else:
+            self.write({'data': store.get(path)})
+
+    def post(self, path, instance=None):
+        '''
+        Handle POST requests.
+        '''
+        try:
+            store = self.manager.stores[instance]
+        except KeyError:
+            logger.warn('unrecognized instance: {}'.format(instance))
+            self.send_error(400, reason='unrecognized instance')
+        else:
+            try:
+                data = json_decode(self.request.body)['data']
+            except (ValueError, KeyError) as e:
+                logger.warn('malformed payload: {}\n{}'.format(e, self.request.body))
+                self.send_error(400, reason='malformed payload')
+            else:
+                store.put(path, data)
+
+    def delete(self, path, instance=None):
+        '''
+        Handle DELETE requests.
+        '''
+        try:
+            store = self.manager.stores[instance]
+        except KeyError:
+            logger.warn('unrecognized instance: {}'.format(instance))
+            self.send_error(400, reason='unrecognized instance')
+        else:
+            store.delete(path)
 
 class PensiveServer(Application):
     '''
@@ -36,8 +96,11 @@ class PensiveServer(Application):
         self._port = port
         self._address = address
 
+        self.manager = StoreManager()
+
         # install handlers for various URLs
-        self.add_handlers(r'.*', [(r'/(.*)', PensiveHandler)])
+        self.add_handlers(r'.*', [(r'/d/(?P<path>.*)', StoreHandler)])
+        self.add_handlers(r'.*', [(r'/i/(?P<instance>.+)/(?P<path>.*)', StoreHandler)])
 
     def log_request(self, handler):
         '''
@@ -70,4 +133,10 @@ class PensiveServer(Application):
             self._address or '*', self._port))
 
         # NOTE: this blocks until the event loop finishes
-        IOLoop.current().start()
+        try:
+            IOLoop.current().start()
+        except KeyboardInterrupt:
+            IOLoop.current().stop()
+
+        logger.info('Pensive stopped on {}:{}'.format(
+            self._address or '*', self._port))
