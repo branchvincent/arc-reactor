4dd90b8aadba116903313fb76e93a9d739eedeb1
Mark Draelos <mark.draelos@duke.edu>
2017-01-08 20:30:37 -0500

    add convenience constant for all record attributes

--- src/log/__init__.py
+++ src/log/__init__.py
@@ -3,5 +3,7 @@ FLOAT_ATTR = ['created', 'msecs', 'relativeCreated']
 STRING_ATTR = ['name', 'pathname', 'filename', 'module', 'funcName', 'threadName', 'processName']
 TEXT_ATTR = ['message']
 
+ALL_ATTR = INTEGER_ATTR + FLOAT_ATTR + STRING_ATTR + TEXT_ATTR
+
 DEFAULT_RECORD_PORT = 7777
 DEFAULT_WEB_PORT = 9999
