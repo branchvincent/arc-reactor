9a3ef00713b7090642e4322867f95991ffc0a409
Mark Draelos <mark.draelos@duke.edu>
2017-02-03 11:59:48 -0500

    add test fixture for master unit tests

--- test/master/test_fsm.py
+++ test/master/test_fsm.py
@@ -6,12 +6,44 @@ from master.fsm import StateMachine
 from states.select_item import SelectItem
 from states.find_item import FindItem
 
+import json
+
 class SimpleFiniteSM(DatabaseDependentTestCase):
     def setUp(self):
         super(SimpleFiniteSM, self).setUp()
 
         self.fsm = StateMachine()
         self.store = self.client.default()
+
+        self.store.put('', json.loads('''
+{
+    "robot": {
+        "selected_item": null
+    },
+
+    "item": {
+        "dr_dobbs_bottle_brush": {
+            "name": "dr_dobbs_bottle_brush",
+            "display_name": "Dr. Dobbs Bottle Brush",
+            "point_value": 10,
+            "location": "binA"
+        },
+        "kong_duck_dog_toy": {
+            "name": "kong_duck_dog_toy",
+            "display_name": "Kong Duck Dog Toy",
+            "point_value": 20,
+            "location": "binA"
+        },
+        "expo_markers": {
+            "name": "expo_markers",
+            "display_name": "Expo Markers",
+            "point_value": 20,
+            "location": "binB"
+        }
+    }
+
+}
+'''))
 
     def test_SelectItem(self):
         self.fsm.add('si1', SelectItem('si1', store=self.store))
