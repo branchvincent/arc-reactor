f965d315e2ea5a8fd705e8ab0853cb958cee2fd4
branchvincent <branchevincent@gmail.com>
2017-02-10 15:15:22 -0500

    Small adjustments to process raw database milestones

--- src/hardware/control/RobotController.py
+++ src/hardware/control/RobotController.py
@@ -4,10 +4,12 @@ import sys; sys.path.append('../..')
 
 import logging; logger = logging.getLogger(__name__)
 from time import sleep
+from math import degrees
 from pensive.client import PensiveClient
 from hardware.tx90l.trajclient.trajclient import TrajClient
 from hardware.vacuum.vpower import PowerUSBStrip, PowerUSBSocket
 
+# List of robot IP addresses
 robots = {  'left': '10.10.1.202',
             'right': '10.10.1.203',
             'local': 'localhost'    }
@@ -15,35 +17,49 @@ robots = {  'left': '10.10.1.202',
 class Robot:
     """A robot defined by its trajectory client"""
     def __init__(self, robot='left', port=1000):
-        print "host ", robots[robot]
-        print "port ", port
+        assert(robot in robots)
         self.client = TrajClient(host=robots[robot], port=port)
         self.name = self.client.name
         self.dof = 6
+        # self.strip = PowerUSBStrip().strips()[0]
+        # self.strip.open()
+        # self.socket = PowerUSBSocket(self.strip,1)
+        # self.socket.power = 'off'
 
 class Trajectory:
-    """A robot trajectory defined by a list of milestones"""
-    def __init__(self, milestones):
+    """A robot trajectory defined by the robot and list of milestones"""
+    def __init__(self, robot, milestones, speed=1):
+        self.robot = robot
         self.milestones = milestones
         self.curr_index = None
         self.curr_milestone = None
+        self.speed = speed
         self.complete = False
-        print milestones
+        # print milestones
 
-    def start(self, robot):
+    def processMilestones(self, milestones):
+        for m in milestones:
+            m[0] /= float(self.speed)
+            m[1]['robot'] = [math.degrees(qi) for qi in m[1]['robot']]
+            m[1]['robot'][2] = ??
+            m[1]['robot'][4] = ??
+        return raw_milestones
+
+    def start(self):
         """Sets the current milestone to the first in the list"""
         self.curr_index = 0
         self.curr_milestone = self.milestones[0]
         for m in self.milestones:
-            dt = m[0]
+            dt = m[0]/float(speed)
             q = m[1]['robot']
-            if len(q) != robot.dof:
+            if len(q) != self.robot.dof:
                 logger.error('Incorrect robot configuration length.')
                 raise Exception('Incorrect robot configuration length.')
-            robot.client.addMilestone(dt,q)
+            self.robot.client.addMilestone(dt,q)
 
-    def update(self, robot):
-        actual_index = len(self.milestones) - robot.client.getCurSegments()
+    def update(self):
+        """Checks the current milestone and updates, if necessary"""
+        actual_index = len(self.milestones) - self.robot.client.getCurSegments()
         if (actual_index != self.curr_index):
             self.advanceMilestone()
             return 'updated'
@@ -64,9 +80,9 @@ class Trajectory:
 
 class RobotController:
     """Trajectory execution for TX90."""
-    def __init__(self, robot='left', port=1000, store=PensiveClient().default()):
+    def __init__(self, robot='left', store=PensiveClient().default(), speed=1):
         # Robot
-        self.robot = Robot(robot,port)
+        self.robot = Robot(robot)
         self.trajectory = None
         self.freq = 100
         # USB hub
@@ -83,26 +99,25 @@ class RobotController:
 
     def updatePlannedTrajectory(self, path='robot/waypoints'):
         """Updates the robot's planned trajectory from the database"""
-        self.trajectory = Trajectory(self.store.get(path))
-
-    def startTrajectoryExecution(self):
-        """Starts to execute the trajectory by adding the milestones to the queue."""
-        self.trajectory.start(self.robot)
+        self.trajectory = Trajectory(self.robot, self.store.get(path))
 
     def updateTrajectoryExecution(self):
         """Monitors and updates the execution of the robot's trajectory"""
         status = self.trajectory.update(self.robot)
         if status == 'updated':
-            # query vaccum
+            # if self.trajectory.vaccumOn():
+            #     self.sock.power = 'on'
+            # else:
+            #     self.socket.power = 'off'
             print "Segment done"
-            pass
 
     def testTriangleWave(self,joint=2,deg=10,period=4):
-        q = self.robot.client.getEndConfig()
-        q2 = q[:]
+        q1 = self.robot.client.getEndConfig()
+        q2, q3 = q[:], q2[:]
         q2[joint] += deg
+        q3[joint] -= 2*deg
 
-        #triangle wave
+        # Triangle wave
         milestones = [
                (period*0.5, {
                   'robot': q2,
@@ -110,10 +125,6 @@ class RobotController:
                   'vaccum': [0]
                 })
             ]
-
-        q3 = q2[:]
-        q3[joint] -= 2*deg
-        print q2[joint]
 
         milestones.append((period, {
                    'robot': q3,
@@ -123,87 +134,31 @@ class RobotController:
             )
 
         milestones.append((period*0.5, {
-                   'robot': q,
+                   'robot': q1,
                    'gripper': [0,0,0],
                    'vaccum': [0]
                  })
             )
 
         self.store.put('robot/waypoints', milestones)
-
         self.run()
-        return
 
     def loop(self):
-        """"""
+        """The loop executed at the given frequency"""
         while not self.trajectory.complete:
             self.updateCurrentConfig()
             self.updateTrajectoryExecution()
             sleep(0.1)
-
         print "Trajectory completed"
 
     def run(self):
-        """"""
+        """Runs the current trajectory in the database"""
         self.updatePlannedTrajectory()
-        self.startTrajectoryExecution()
+        self.trajectory.start()
         self.loop()
-
-        # curr_milestone = self.robot.getCurSegments()
-        # milestones = self.robot.getRemainingSegments()
-        # print "current milestone", curr_milestone
-        # print "milestones ", milestones
+        # print "current milestone", self.robot.getCurSegments()
+        # print "milestones ",  self.robot.getRemainingSegments()
         # print "max segments ", self.robot.getMaxSegments()
-
-# class Trajectory:
-#     """A robot trajectory defined by a list of milestones"""
-#     def __init__(self, milestones, robot):
-#         self.milestones = milestones
-#         self.curr_index = None
-#         self.curr_milestone = None
-#         self.robot = robot
-#     #
-#     # def start(self):
-#     #     """Starts to execute the trajectory by adding the milestones to the queue."""
-#     #     self.curr_index = 0
-#     #     self.curr_milestone = self.milestones[0]
-#     #     for m in self.milestones:
-#     #         dt = m[0]
-#     #         q = m[1]['robot']
-#     #         if len(q) != robot.dof:
-#     #             logger.error('Incorrect robot configuration length.')
-#     #             raise Exception('Incorrect robot configuration length.')
-#     #         self.robot.client.addMilestone(dt,q)
-#     #
-#     # def update(self):
-#     #     """Monitors and updates the execution of the trajectory"""
-#     #     actual_milestone = robot.client.getCurSegments()
-#     #     belief_milestone = [self.curr_milestone[0], self.curr_milestone[1]['robot']]
-#     #     if (actual_milestone != belief_milestone):
-#     #         success = self.advanceMilestone()
-#     #         return success
-#     #     else:
-#     #         return true
-#     #
-#     # def advanceMilestone(self):
-#     #     """Advances to the next milestone, if applicable"""
-#     #     self.curr_index += 1
-#     #     if (self.curr_index < len(self.milestones)):
-#     #         self.curr_milestone = self.milestones[self.curr_index]
-#     #         if self.milestones[1]['vaccumm']
-#     #         return true
-#     #     else:
-#     #         self.curr_index = None
-#     #         self.curr_milestone = None
-#     #         return false
-#     #
-#     # def end(self):
-#     #     """"""
-#     #     pass
-#     #
-#     # def run(self):
-#     #     self.start()
-#     #     self.update()
 
 if __name__ == "__main__":
     store = PensiveClient(host='http://10.10.1.102:8888/').default()
