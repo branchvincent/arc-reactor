f8c38f8052d7430b0af573a7d3afaef8dcc80551
Mark Draelos <mark.draelos@duke.edu>
2017-02-12 17:44:39 -0500

    improve motion plan checkpoint

--- src/checkpoint/motion_plan.py
+++ src/checkpoint/motion_plan.py
@@ -21,20 +21,25 @@ class WorldViewer(GLRealtimeProgram):
         GLRealtimeProgram.__init__(self, 'Motion Plan Checkpoint')
 
         store = PensiveClient().default()
+        self.world = build_world(store)
+
+        self.robot_path = []
         self.motion_plan = store.get('/robot/waypoints')
-        t = 1
+
+        t = 0
         for waypoint in self.motion_plan:
             dt = waypoint[0]
             waypoint[0] = t
             t += dt
 
-        self.world = build_world(store)
-
         self.fps = 30.0
         self.dt = 1 / self.fps
 
+        self.speed_scale = 0.1
+        self.end_delay = 1
+
         self.n = 0
-        self.start_time = None
+        self.start_time = -self.end_delay
         self.waypoint_index = None
 
     def display(self):
@@ -49,36 +54,38 @@ class WorldViewer(GLRealtimeProgram):
         if not self.motion_plan:
             return
 
-        if self.start_time is None:
-            self.start_time = time()
-
-        current_time = time() - self.start_time
-
-        # find the waypoint before and after the current time
-        segment = None
-        for i in range(1, len(self.motion_plan)):
-            if self.motion_plan[i][0] < current_time:
-                continue
-
-            segment = self.motion_plan[i-1:i+1]
-            break
-        else:
-            if current_time > self.motion_plan[-1][0] + 1:
-                self.start_time = None
-                return
+        current_time = self.speed_scale * (time() - self.start_time)
+        if current_time > self.motion_plan[-1][0] + self.end_delay:
+            self.start_time = time() + self.end_delay
 
         command = {}
 
-        # first-order hold for continuous variables
-        ratio = (segment[0][0] - current_time) / (segment[1][0] - segment[0][0])
-        for name in ['robot', 'shelf', 'gripper']:
-            if name in segment[0][1] and name in segment[1][1]:
-                command[name] = [(1-ratio)*a + ratio*b for (a, b) in zip(segment[0][1][name], segment[1][1][name])]
+        if current_time < 0:
+            # before start
+            command = self.motion_plan[0][1]
+        elif current_time > self.motion_plan[-1][0]:
+            # after end
+            command = self.motion_plan[-1][1]
+        else:
+            # find the waypoint before and after the current time
+            segment = None
+            for i in range(1, len(self.motion_plan)):
+                if self.motion_plan[i][0] < current_time:
+                    continue
 
-        # zero-order hold for discrete variables
-        for name in ['vacuum']:
-            if name in segment[0][1]:
-                command[name] = segment[0][1][name]
+                segment = self.motion_plan[i-1:i+1]
+                break
+
+            # first-order hold for continuous variables
+            ratio = (segment[0][0] - current_time) / (segment[1][0] - segment[0][0])
+            for name in ['robot', 'shelf', 'gripper']:
+                if name in segment[0][1] and name in segment[1][1]:
+                    command[name] = [ratio*a + (1 - ratio)*b for (a, b) in zip(segment[0][1][name], segment[1][1][name])]
+
+            # zero-order hold for discrete variables
+            for name in ['vacuum']:
+                if name in segment[0][1]:
+                    command[name] = segment[0][1][name]
 
         if 'robot' in command:
             self.world.robot('tx90l').setConfig(command['robot'])
