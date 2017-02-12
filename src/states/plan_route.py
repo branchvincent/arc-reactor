2093c2ed2c66efd374dbae363ce896d7e0477e48
Mark Draelos <mark.draelos@duke.edu>
2017-02-12 17:03:29 -0500

    integrate motion planner in PlanRoute state

--- src/states/plan_route.py
+++ src/states/plan_route.py
@@ -1,14 +1,49 @@
+import logging
+
 from master.fsm import State
+from master.world import build_world
+
+from motion import planner
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 class PlanRoute(State):
     def run(self):
-        self.startLoc = self.store.get('/robot/current_config')
+        world = build_world(self.store, ignore=['camera'])
 
-        self.endLoc = self.store.get('/robot/goal_config')        
-        #compute route
-        
-        #hopefully get to the goal
-        #this is a placeholder for now
-        self.store.put('/robot/current_config', self.endLoc)
-        self.store.put('/status/route_plan', True)  
+        item = self.store.get('/robot/selected_item')
+        item_pose = self.store.get('/item/{}/pose'.format(item))
+        shelf_pose = self.store.get('/shelf/pose')
 
+        target_item = {
+            'position': shelf_pose.dot(item_pose)[:3, 3].tolist(),
+            'vacuum_offset': [0, 0, 0.09],
+            'drop offset': 0.1
+        }
+
+        box = self.store.get('/robot/selected_box')
+        box_pose = self.store.get('/box/{}/pose'.format(box))
+
+        target_box = {
+            'position': box_pose[:3, 3].tolist(),
+            'drop position': (box_pose[:3, 3] + [0, 0, 0.1]).tolist()
+        }
+
+        logger.info('planning route for "{}" to "{}"'.format(item, box))
+
+        # compute route
+        try:
+            motion_plan = planner.pick_up(world, target_item, target_box)
+
+            if not motion_plan:
+                raise RuntimeError('motion plan is empty')
+        except Exception:
+            self.store.put('/status/route_plan', False)
+            logger.exception('failed to generation pick motion plan')
+        else:
+            self.store.put('/robot/waypoints', motion_plan)
+            self.store.put('/status/route_plan', True)
+            logger.info('route generated')
+
+if __name__ == '__main__':
+    PlanRoute('pr').run()
