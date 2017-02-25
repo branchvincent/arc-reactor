42fcab4b752905c00792ebfb8487d258f7f3adeb
Mark Draelos <mark.draelos@duke.edu>
2017-02-25 15:49:19 -0500

    update FindItem state to use live camera images

--- src/states/find_item.py
+++ src/states/find_item.py
@@ -1,5 +1,8 @@
 from master.fsm import State
 from simulation.grabcut import GrabObject
+
+from hardware.SR300 import DepthCameras
+
 import cv2
 import numpy
 import logging; logger = logging.getLogger(__name__)
@@ -10,15 +13,34 @@ class FindItem(State):
         self.lastLoc = self.ItemDict['location']
 
         if self.store.get('/simulate/object_detection'):
-            self.grab = GrabObject()
-            self.grab.run() #output to db as mask
+            logger.info('simulating object detection')
+
+            # acquire a camera image
+            cam = DepthCameras()
+            if not cam.connect():
+                raise RuntimeError('failed accessing camera')
+
+            (images, serial) = cam.acquire_image(0)
+            if not serial:
+                raise RuntimeError('camera acquisition failed')
+            
+            (color, aligned_color, _, _, _, self.pc) = images
+
+            logger.debug('acquired image from camera {}'.format(serial))
+            self.store.put('/camera/camera1/color_image', color)
+            self.store.put('/camera/camera1/aligned_color_image', self.pc)
+
+            # perform grab cut selection on color-aligned-to-depth image
+            self.grab = GrabObject(images[1])
+            self.mask = self.grab.run() 
+
             cv2.destroyAllWindows()
-            self.mask = self.store.get('/camera/camera1/mask')
-            self.pc = self.store.get('/camera/camera1/point_cloud')
-            self.binaryMask = (self.mask.sum(axis=2)>0)
+
+            self.binaryMask = (self.mask.sum(axis=2) > 0)
             #cv2.imwrite('test/checkMask.png', self.binaryMask)
-            self.obj_pc = self.pc[self.binaryMask]
+            self.obj_pc = self.pc[numpy.bitwise_and(self.binaryMask, self.pc[:, :, 2] > 0)]
             self.store.put('/item/'+self.ItemDict['name']+'/point_cloud', self.obj_pc)
+            logger.debug('found {} object points'.format(self.obj_pc.shape[0]))
 
             #fix camera tmp
             self.cam_pose = self.store.get('/camera/camera1/pose')
@@ -30,10 +52,14 @@ class FindItem(State):
             self.shelf = self.store.get('/shelf/pose')
 
             #update pose as mean of obj pc
-            self.tmp = self.obj_pc[:,2]>0
-            self.pose = numpy.eye(4,4)
-            self.pose[:3,3]=self.obj_pc[self.tmp].mean(axis=0)
-            self.store.put('/item/'+self.ItemDict['name']+'/pose', numpy.linalg.inv(self.shelf).dot(self.pose))
+            self.pose = numpy.eye(4, 4)
+            self.pose[:3, 3] = self.obj_pc.mean(axis=0)
+            logger.debug('object pose relative to camera\n{}'.format(self.pose))
+
+            obj_pose = numpy.linalg.inv(self.shelf).dot(self.pose)
+            logger.debug('object pose relative to shelf\n{}'.format(obj_pose))
+
+            self.store.put(['item', self.ItemDict['name'], 'pose'], obj_pose)
 
         else:
             # for all cameras available, do:
@@ -53,3 +79,5 @@ class FindItem(State):
         self.store.put('/status/selected_item_location', True)
         #etc
 
+if __name__ == '__main__':
+    FindItem('fi').run()
