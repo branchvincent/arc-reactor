862e5013236e12000bc8835d439645968488bff1
bk <bnk5@duke.edu>
2017-03-29 16:45:38 -0400

    Changed name and removed unused inputs for make3dmodel.

--- src/perception/take_images.py
+++ src/perception/take_images.py
@@ -10,12 +10,10 @@ from perceptionGUI import GLWidget
 from calibration import VideoThread
 import logging
 logger = logging.getLogger()
-class MakeModelsGUI(QtWidgets.QWidget):
+class TakeImagesGUI(QtWidgets.QWidget):
     
     def __init__(self):
-        super(MakeModelsGUI, self).__init__()
-        self.openGLWidget = GLWidget(self)
-        self.openGLWidget.setMinimumSize(640,480)
+        super(TakeImagesGUI, self).__init__()
         ctx = rs.context()
         self.cam0 = VideoThread(ctx, 0)
         self.cam1 = VideoThread(ctx, 1)
@@ -43,46 +41,20 @@ class MakeModelsGUI(QtWidgets.QWidget):
         self.horzBot = QtWidgets.QHBoxLayout()
 
         #top
-        self.take_image_button = QtWidgets.QPushButton("Take image", self)
-        self.remove_last_image_button = QtWidgets.QPushButton("Remove last image", self)
-        self.save_button = QtWidgets.QPushButton("Save", self)
+    
         self.video_capture_button = QtWidgets.QPushButton("Video Capture", self)
-        self.take_image_button.setMaximumWidth(75)
         self.video_capture_button.setMaximumWidth(105)
-        self.remove_last_image_button.setMaximumWidth(125)
-        self.save_button.setMaximumWidth(75)
-        self.take_image_button.show()
         self.video_capture_button.show()
-        self.remove_last_image_button.show()
-        self.save_button.show()
         self.horzTop.addStretch(1)
         self.horzTop.addWidget(self.video_capture_button)
-        self.horzTop.addSpacing(20)
-        self.horzTop.addWidget(self.take_image_button)
-        self.horzTop.addSpacing(20)
-        self.horzTop.addWidget(self.remove_last_image_button)
-        self.horzTop.addSpacing(20)
-        self.horzTop.addWidget(self.save_button)
         self.horzTop.addStretch(1)
-
-        #disable unused buttons
-        self.take_image_button.setEnabled(False)
-        self.remove_last_image_button.setEnabled(False)
-        self.save_button.setEnabled(False)
-
+        
         #connections 
-        self.take_image_button.pressed.connect(self.take_images)
         self.video_capture_button.pressed.connect(self.start_video_capture)
 
         #mid
-        self.rotx_text = QtWidgets.QTextEdit(self)
-        self.roty_text = QtWidgets.QTextEdit(self)
-        self.rotz_text = QtWidgets.QTextEdit(self)
         self.objectNameTextBox = QtWidgets.QLineEdit(self)
         self.objectNameTextBox.editingFinished.connect(self.updateObjectName)
-        self.rotx_text.setMaximumHeight(35)
-        self.roty_text.setMaximumHeight(35)
-        self.rotz_text.setMaximumHeight(35)
         self.objectNameTextBox.setMaximumHeight(35)
 
         self.horzMid.addStretch(1)
@@ -92,58 +64,32 @@ class MakeModelsGUI(QtWidgets.QWidget):
         self.horzMid.addWidget(self.objectNameTextBox)
         self.horzMid.addSpacing(30)
 
-        label = QtWidgets.QLabel(self)
-        label.setText("X rotation")
-        self.horzMid.addWidget(label)
-        self.horzMid.addWidget(self.rotx_text)
-        self.horzMid.addSpacing(10)
-
-        label = QtWidgets.QLabel(self)
-        label.setText("Y rotation")
-        self.horzMid.addWidget(label)
-        self.horzMid.addWidget(self.roty_text)
-        self.horzMid.addSpacing(10)
-        
-        label = QtWidgets.QLabel(self)
-        label.setText("Z rotation")
-        self.horzMid.addWidget(label)
-        self.horzMid.addWidget(self.rotz_text)
-
         self.horzMid.addStretch(1)
         
 
         #bot
-        self.vertBot = QtWidgets.QVBoxLayout()
         self.camera_display0 = QtWidgets.QLabel(self)
         self.camera_display0.show()
         zeroImg = np.zeros((480,640,3),dtype='uint8')
         image = QtGui.QImage(zeroImg, zeroImg.shape[1], zeroImg.shape[0], zeroImg.shape[1] * 3,QtGui.QImage.Format_RGB888)
         pix = QtGui.QPixmap(image)
-        self.camera_display0.setPixmap(pix.scaled(320, 240))
+        self.camera_display0.setPixmap(pix.scaled(640,480))
 
         self.camera_display1 = QtWidgets.QLabel(self)
         self.camera_display1.show()
         zeroImg = np.zeros((480,640,3),dtype='uint8')
         image = QtGui.QImage(zeroImg, zeroImg.shape[1], zeroImg.shape[0], zeroImg.shape[1] * 3,QtGui.QImage.Format_RGB888)
         pix = QtGui.QPixmap(image)
-        self.camera_display1.setPixmap(pix.scaled(320, 240))
+        self.camera_display1.setPixmap(pix.scaled(640,480))
 
-        self.vertBot.addWidget(self.camera_display0)
-        self.vertBot.addSpacing(20)
-        self.vertBot.addWidget(self.camera_display1)
-
-        self.horzBot.addWidget(self.openGLWidget)
+        self.horzBot.addWidget(self.camera_display0)
         self.horzBot.addSpacing(20)
-        self.horzBot.addItem(self.vertBot)
+        self.horzBot.addWidget(self.camera_display1)
 
         #add all layouts to the vertical layout
         self.vertLayout.addItem(self.horzTop)
         self.vertLayout.addItem(self.horzMid)
         self.vertLayout.addItem(self.horzBot)
-
-
-    def take_images(self):
-        pass
 
     def updateObjectName(self):
         self.obj_name = self.objectNameTextBox.text()
@@ -159,7 +105,7 @@ class MakeModelsGUI(QtWidgets.QWidget):
             return
         else:
             self.timer.setSingleShot(True)
-            self.timer.setInterval(5000)
+            self.timer.setInterval(10000)
             self.timer.start()
         self.cam0.keepRunning = True
         while not self.cam0.can_emit:
@@ -185,12 +131,12 @@ class MakeModelsGUI(QtWidgets.QWidget):
         pix_img = QtGui.QImage(image, image.shape[1], image.shape[0], image.shape[1] * 3,QtGui.QImage.Format_RGB888)
         pix = QtGui.QPixmap(pix_img)
         if cam_num == 0:
-            self.camera_display0.setPixmap(pix.scaled(320,240))
+            self.camera_display0.setPixmap(pix.scaled(640,480))
             self.cam0.mutex.acquire()
             self.cam0.can_emit = True
             self.cam0.mutex.release()
         elif cam_num == 1:
-            self.camera_display1.setPixmap(pix.scaled(320,240))
+            self.camera_display1.setPixmap(pix.scaled(640,480))
             self.cam1.mutex.acquire()
             self.cam1.can_emit = True
             self.cam1.mutex.release()
@@ -209,7 +155,7 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
 
     #set up the window
-    m3 = MakeModelsGUI()
+    m3 = TakeImagesGUI()
     sys.exit(app.exec_())
 
 
