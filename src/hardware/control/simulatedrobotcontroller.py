3bbd7c359024dc356eecc97697a0fceb59949b74
Branch Vincent <bev5@duke.edu>
2017-04-03 14:14:31 -0400

    Added jog method to simulated robot controller

--- src/hardware/control/simulatedrobotcontroller.py
+++ src/hardware/control/simulatedrobotcontroller.py
@@ -5,10 +5,15 @@ import logging; logger = logging.getLogger(__name__)
 from time import sleep, time
 from copy import deepcopy
 from math import pi, degrees, radians
-from math import degrees, radians, isnan, isinf
+from math import degrees, radians, isnan, isinf, ceil
 from pensive.client import PensiveClient
 from hardware.tx90l.trajclient.trajclient import TrajClient
 from hardware.vacuum import Vacuum
+import numpy as np
+
+from master.world import build_world
+from klampt.model.collide import WorldCollider
+from klampt.plan.robotcspace import RobotCSpace
 
 # List of robot IP addresses
 dof = 6
@@ -22,6 +27,35 @@ def Assert(condition, message):
     if not condition:
         logger.error(message)
         raise Exception(message)
+
+def createMilestoneMap(dt,qs,type='db',to='robot'): #degs=False,
+    Assert(type in ['robot','db'], 'Unrecognized option: "{}"'.format(type))
+    Assert(to in ['robot','db'], 'Unrecognized option: "{}"'.format(to))
+    if type == 'db' and to == 'robot':
+        qs = [[degrees(qi) for qi in q] for q in qs]
+        qs = [q[1:] for q in qs]
+        for q in qs:
+            q[2] *= -1
+            q[4] *= -1
+    elif type =='robot' and to == 'db':
+        qs = [[radians(qi) for qi in q] for q in qs]
+        qs = [[0] + q for q in qs]
+        for q in qs:
+            q[3] *= -1
+            q[5] *= -1
+    elif type == to:
+        pass
+    # if type == 'db':
+    #     if degs:
+    #         qs = [[radians(qi) for qi in q] for q in qs]
+    #     if len(qs) > 0 and len(qs[0]) == dof:
+    #         qs = [[0] + q for q in qs]
+    # elif type == 'robot':
+    #     if not degs:
+    #         qs = [[degrees(qi) for qi in q] for q in qs]
+    #     if len(qs) > 0 and len(qs[0]) == dof + 1:
+    #         qs = [q[1:] for q in qs]
+    return [[dt,{'robot': qi}] for qi in qs]
 
 def convertConfigToRobot(q_old, speed):
     """Converts database-style configuration to robot-stye configuration"""
@@ -135,8 +169,54 @@ class SimulatedRobotController:
         q = convertConfigToDatabase(q[1]['robot'])
         self.store.put(path, q)
 
-# if __name__ == "__main__":
-#     store = PensiveClient().default()
-#     c = SimulatedRobotController(store=store)
-    # store.put('robot/waypoints', sample_milestones)
-    # c.run()
+    def jogTo(self,qdes,rads=True):
+        """Jogs the robot to the specified configuration, in radians"""
+        # Setup world and cspace
+        world = build_world(self.store)
+        robotSim = world.robot('tx90l')
+        collider = WorldCollider(world)
+        cspace = RobotCSpace(robotSim,collider=collider)
+
+        # Convert configuration
+        if not rads:
+            qdes = [radians(qi) for qi in qdes]
+        q0 = self.store.get('robot/current_config')
+
+        # Determine discretization
+        dq_max, dv_max = radians(5.), radians(60.)
+        distance = robotSim.distance(q0,qdes)
+        numMilestones = 1 + ceil(distance/dq_max)
+        dt = 1.5 * dq_max/dv_max
+        logger.info("Jogging from {} to {}, with {} milestones".format(q0,qdes,numMilestones))
+
+        # Create milestones, checking for feasibility
+        qs = [robotSim.interpolate(q0,qdes,ui) for ui in np.linspace(0,1,numMilestones)]
+        feasible = True
+        for qi in qs:
+            if not cspace.inJointLimits(qi):
+                logger.warn("Configuration not in joint limits")
+                feasible = False
+                break
+            elif cspace.selfCollision(x=qi):
+                logger.warn("Configuration colliding with self")
+                feasible = False
+                break
+            elif cspace.envCollision(x=qi):
+                logger.warn("Configuration colliding with environment")
+                feasible = False
+                break
+
+        # Run path, if feasible
+        if feasible:
+            milestones = createMilestoneMap(dt,qs,type='db',to='db')
+            self.trajectory = SimulatedTrajectory(milestones=milestones)
+            self.run()
+        else:
+            logger.warn("Jogger could not find feasible path")
+
+
+if __name__ == "__main__":
+    store = PensiveClient().default()
+    c = SimulatedRobotController(store=store)
+    m = [0]*7
+    c.jogTo(m)
