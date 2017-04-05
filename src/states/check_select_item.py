e1ff6e0cbb6445bf3478a42c68e96eae430295bd
Mark Draelos <mark.draelos@duke.edu>
2017-04-04 23:43:24 -0400

    UI tweak to SelectItem checkpoint

--- src/states/check_select_item.py
+++ src/states/check_select_item.py
@@ -40,7 +40,7 @@ class ItemSelector(QWidget):
         layout.addWidget(label, 0, 0, 1, width)
 
         for (i, item) in enumerate(sorted(items)):
-            parts = items[item]['name'].split(' ') + ['({})'.format(items[item]['location'])]
+            parts = items[item].get('display_name', items[item]['name']).split(' ') + ['({})'.format(items[item]['location'])]
             lines = ['']
             for part in parts:
                 if len(lines[-1]) > 10:
