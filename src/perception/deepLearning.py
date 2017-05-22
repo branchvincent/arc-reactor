7255634d5b75ca1b9d18577c4de5d2b614ed744c
bk <bnk5@duke.edu>
2017-05-21 20:08:24 -0400

    Changed name of class and removed prep_image since we dont use it

--- src/perception/deepLearning.py
+++ src/perception/deepLearning.py
@@ -16,7 +16,7 @@ from lasagne.utils import floatX
 import logging
 logger = logging.getLogger(__name__)
 
-class ObjectRecognizer:
+class DeepLearningRecognizer:
     '''
     Class for performing object recognition. Uses theano and pretrained net to guess object
     '''
@@ -69,33 +69,6 @@ class ObjectRecognizer:
         d = pickle.load(open(filename, 'rb'),encoding='latin-1')
         logger.info("Weights loaded from file {}".format(filename))
         return d
-
-    def prep_image(self, im):
-        '''
-        expects a numpy array not a filename
-        '''
-        IMAGE_MEAN = self.weights['mean value'][:, np.newaxis, np.newaxis]
-        # Resize so smallest dim = 256, preserving aspect ratio
-        h, w, _ = im.shape
-        if h < w:
-            im = skimage.transform.resize(im, (256, w * 256 // h), preserve_range=True)
-        else:
-            im = skimage.transform.resize(im, (h * 256 // w, 256), preserve_range=True)
-        # Central crop to 224x224
-        h, w, _ = im.shape
-        im = im[h // 2 - 112:h // 2 + 112, w // 2 - 112:w // 2 + 112]
-
-        rawim = np.copy(im).astype('uint8')
-
-        # Shuffle axes to c01
-        im = np.swapaxes(np.swapaxes(im, 1, 2), 0, 1)
-
-        # discard alpha channel if present
-        im = im[:3]
-        # Convert to BGR
-        im = im[::-1, :, :]
-        # im = im - IMAGE_MEAN
-        return rawim, floatX(im[np.newaxis])
 
     def guessObject(self, images):
         '''
