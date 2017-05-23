e7696fc3b4d473706632f2cb88ef592a84272fcd
Mark Draelos <mark.draelos@duke.edu>
2017-05-23 15:22:56 -0400

    add utility for showing images in the database

---
+++ src/util/show.py
@@ -0,0 +1,59 @@
+import numpy
+
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot, cm
+
+def show(data, format=None):
+    if data.dtype == numpy.int32:
+        # guess segmentation data
+        labels = data
+        # HACK: not sure why cm.Set1.colors stopped working
+        cmap = cm.Set1._segmentdata['blue']
+
+        labels_rgb = 255 * numpy.array(cmap)[(labels - 1) % len(cmap)]
+        labels_rgb[labels == 0] = [0, 0, 0]
+
+        pyplot.imshow(labels_rgb)
+
+    elif data.dtype == numpy.uint8:
+        # guess image data
+        image = data
+
+        pyplot.imshow(data)
+
+    else:
+        print 'unknown data type'
+
+    pyplot.show()
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+    parser = ArgumentParser(description='world file generation', formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-a', '--address', metavar='HOST', help='database server host')
+    parser.add_argument('-s', '--store', metavar='STORE', help='database store')
+    parser.add_argument('-p', '--path', metavar='PATH', help='path to a JSON database')
+    parser.add_argument('url', metavar='URL', help='url to image')
+
+    args = parser.parse_args()
+
+    if args.path:
+        # load the JSON object
+        from pensive.client import json_decode
+        obj = json_decode(open(args.path).read())
+
+        # populate in-memory store
+        from pensive.core import Store
+        store = Store(obj)
+
+    else:
+        # connect to the database
+        from pensive.client import PensiveClient
+        client = PensiveClient(args.address)
+
+        # get the store
+        store = client.store(args.store)
+
+    show(store.get(args.url))
