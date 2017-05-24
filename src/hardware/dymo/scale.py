0639894c6de279bda9b7bb2e25ce27596dfbcddd
branchvincent <branchevincent@gmail.com>
2017-05-24 10:12:22 -0400

    Adding port to scale constructor

--- src/hardware/dymo/scale.py
+++ src/hardware/dymo/scale.py
@@ -19,31 +19,24 @@ DATA_MODE_OUNCES = 11
 OUNCES_2_GRAMS = 28.3495231
 # GRAMS_2_OUNCES = 1/OUNCES_2_GRAMS
 
-# def oz2g(oz):
-#     return oz * OUNCES_2_GRAMS
-# def g2oz(g):
-#     return g * GRAMS_2_OUNCES
-
 class Scale:
-    def __init__(self, serial, store=None):
+    def __init__(self, serial, port, store=None):
         self.store = store or PensiveClient().default()
         # Connect or simulate
-        if self.store.get('/simulate/scale', True):
+        if self.store.get('/simulate/scale', False):
             self.device = None
-            logger.warn('Scale simulated')
+            logger.warn('Scale {} simulated'.format(serial))
         else:
-            self.connect(serial)
-            logger.info('Scale connected')
+            self.connect(serial, port)
+            logger.info('Scale {} connected'.format(serial))
 
-    def connect(self, serial):
-        # self.device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID, find_all=True)
-        # if self.device is None:
-        #     raise RuntimeError('Could not find device')
-
-        # Find by serial
+    def connect(self, serial, port):
+        # Find by serial and port
         self.device = None
+        self.serial = serial
         for device in usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID, find_all=True):
-            if usb.util.get_string(device, 256, device.iSerialNumber) == serial:
+            # self.getInfo(device)
+            if usb.util.get_string(device, device.iSerialNumber) == serial and device.port_number == port:
                 self.device = device
                 break
         if self.device is None:
@@ -51,7 +44,7 @@ class Scale:
 
         # Claim
         if self.device.is_kernel_driver_active(0):
-            self.device.dettach_kernel_driver(0)
+            self.device.detach_kernel_driver(0)
         # Set config
         self.device.set_configuration()
 
@@ -70,11 +63,11 @@ class Scale:
         endpoint = self.device[0][(0,0)][0]
         data = None
         while data is None and attempts > 0:
-            logger.info('{} attempts remaining'.format(attempts))
             try:
                 data = self.device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
             except usb.core.USBError as e:
-                logger.exception('Error')
+                logger.exception('Scale had an error')
+                logger.info('{} attempts remaining'.format(attempts))
                 data = None
                 attempts -= 1
                 # if e.args == ('Operation timed out',):
@@ -96,11 +89,29 @@ class Scale:
         raw_weight = data[4] + data[5] * 256
         scaling_factor = 0.01 #data[3]
         if data[2] == DATA_MODE_OUNCES:
-            return (raw_weight * scaling_factor) * OUNCES_2_GRAMS/1000
+            return (raw_weight * scaling_factor) * OUNCES_2_GRAMS/1000.
         elif data[2] == DATA_MODE_GRAMS:
-            return raw_weight/1000
+            return raw_weight/1000.
+
+    def getInfo(self, dev, all=False):
+        print 'Found:'
+        print '\tSerial {}'.format(usb.util.get_string(dev, dev.iSerialNumber))
+        print '\tPort {}'.format(dev.port_number)
+        print '\tDescription {}'.format(usb.util.get_string(dev, dev.iProduct))
+        print '\tManufacturer {}'.format(usb.util.get_string(dev, dev.iManufacturer))
+        if all:
+            for attrib in dir(dev):
+                if not attrib.startswith('_') and not attrib == 'configurations':
+                    x=getattr(dev, attrib)
+                    print "  ", attrib, x
+            for config in dev.configurations:
+                for attrib in dir(config):
+                    if not attrib.startswith('_'):
+                        x=getattr(config, attrib)
+                        print "    ", attrib, x
 
 if __name__ == '__main__':
-    s = Scale(1081610012952)
+    s = Scale(serial='0071431044934', port=3)
+    # 0071431044934
     weight = s.read()
     print "Weight: {} kg".format(weight)
