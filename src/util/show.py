72fb2ebcf147b96be1d7a402f184a6f3bdbc806a
Mark Draelos <mark.draelos@duke.edu>
2017-05-27 17:02:17 -0400

    handle multiple URLs together in database image utility

--- src/util/show.py
+++ src/util/show.py
@@ -4,43 +4,52 @@ import matplotlib
 matplotlib.use('Qt4Agg')
 from matplotlib import pyplot, cm
 
-def show(data, fmt=None):
-    if not fmt:
-        if len(data.shape) == 2:
-            if data.dtype == numpy.int32:
-                fmt = 'label'
-            else:
-                fmt = 'intensity'
-        elif len(data.shape) == 3 and data.shape[2] == 3:
-            if data.dtype == numpy.uint8:
-                fmt = 'color'
-            elif data.dtype == numpy.float32:
-                fmt = 'point_cloud'
+def show(store, urls, fmt=None):
+    for url in urls:
+        pyplot.figure()
 
-    if not fmt:
-        raise RuntimeError('unknown data format')
+        print url, '->',
+        data = store.get(url)
+        print data.shape, data.dtype
 
+        if not fmt:
+            if len(data.shape) == 2:
+                if data.dtype == numpy.int32:
+                    fmt = 'label'
+                else:
+                    fmt = 'intensity'
+            elif len(data.shape) == 3 and data.shape[2] == 3:
+                if data.dtype == numpy.uint8:
+                    fmt = 'color'
+                elif data.dtype == numpy.float32:
+                    fmt = 'point_cloud'
 
-    if fmt == 'label':
-        pyplot.imshow(data, cmap=cm.viridis)
-        pyplot.colorbar()
+        if not fmt:
+            raise RuntimeError('unknown data format')
 
-    elif fmt == 'color':
-        pyplot.imshow(data)
+        if fmt == 'label':
+            pyplot.imshow(data, cmap=cm.viridis)
+            pyplot.colorbar()
+            pyplot.title(url)
 
-    elif fmt == 'intensity':
-        pyplot.imshow(data, cmap=cm.gray)
-        pyplot.colorbar()
+        elif fmt == 'color':
+            pyplot.imshow(data)
+            pyplot.title(url)
 
-    elif fmt == 'point_cloud':
-        cloud = data.reshape((-1, 3))
+        elif fmt == 'intensity':
+            pyplot.imshow(data, cmap=cm.gray)
+            pyplot.colorbar()
+            pyplot.title(url)
 
-        from util import pcd
-        path = '/tmp/cloud.pcd'
-        pcd.write(cloud, path)
+        elif fmt == 'point_cloud':
+            cloud = data.reshape((-1, 3))
 
-        from subprocess import call
-        call(['pcl_viewer', '-ax', '0.1', path])
+            from util import pcd
+            path = '/tmp/cloud.pcd'
+            pcd.write(cloud, path)
+
+            from subprocess import Popen
+            Popen(['pcl_viewer', '-ax', '0.1', path])
 
     pyplot.show()
 
@@ -53,7 +62,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--store', metavar='STORE', help='database store')
     parser.add_argument('-p', '--path', metavar='PATH', help='path to a JSON database')
     parser.add_argument('-f', '--format', metavar='FORMAT', help='data display format', choices=['label', 'intensity', 'color', 'point_cloud', None])
-    parser.add_argument('url', metavar='URL', help='url to image')
+    parser.add_argument('url', nargs='+', metavar='URL', help='url to image')
 
     args = parser.parse_args()
 
@@ -74,4 +83,4 @@ if __name__ == '__main__':
         # get the store
         store = client.store(args.store)
 
-    show(store.get(args.url), args.format)
+    show(store, args.url, args.format)
