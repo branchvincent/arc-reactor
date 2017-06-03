fe1a9ee563d4162485c15ae1da55609154821ab5
branchvincent <branchevincent@gmail.com>
2017-06-03 15:57:30 -0400

    Updated PlanStowGrab state

--- src/states/plan_stow_grab.py
+++ src/states/plan_stow_grab.py
@@ -1,32 +1,38 @@
+from master.fsm import State
+from master.world import build_world, rpy
+from motion.new_planner import StowPlanner
+from util.math_helpers import build_pose, transform, rotate, normalize
+
 import logging
 import numpy
 from time import time
-from master.fsm import State
-from master.world import build_world
-from motion.new_planner import StowPlanner
+from math import pi
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 class PlanStowGrab(State):
     def run(self):
-        
+
+        # Get item and grasp info
         item = self.store.get('/robot/selected_item')
         grasp = self.store.get('/robot/selected_grasp')
-        # item is in stow tote
+        # TODO: add to db (defaults to vacuum for now)
+        gripper = self.store.get('/robot/active_gripper', 'vacuum').lower()
+        if gripper not in ['vacuum', 'mechanical']:
+            raise RuntimeError('unrecognized gripper "{}"'.format(gripper))
+
+        logger.info('planning route for "{}" to stow tote'.format(item))
+
+        # Get item location (must be stow tote)
         location = self.store.get(['item', item, 'location'])
+        if location not in ['stow_tote', 'stow tote']:
+            raise RuntimeError('unrecognized item location: "{}"'.format(location))
+        reference_pose = self.store.get('/tote/stow/pose')
 
-        logger.info('planning route for "{}" to shelf'.format(item))
-
-        world = build_world(self.store)
-        self.world = world
-
+        # Calculate item bounding box
+        self.world = build_world(self.store)
         item_pose_local = self.store.get(['item', item, 'pose'])
         item_pc_local = self.store.get(['item', item, 'point_cloud'])
-
-#        if location in ['stow_tote', 'stow tote']:
-            reference_pose = self.store.get('/tote/stow/pose')
-#        else:
-#            raise RuntimeError('unrecognized item location: "{}"'.format(location))
 
         item_pose_world = reference_pose.dot(item_pose_local)
         item_pc_world = item_pc_local.dot(item_pose_world[:3, :3].T) + item_pose_world[:3, 3].T
@@ -35,8 +41,10 @@ class PlanStowGrab(State):
             [item_pc_world[:, 0].min(), item_pc_world[:, 1].min(), item_pc_world[:, 2].max()],
             [item_pc_world[:, 0].max(), item_pc_world[:, 1].max(), item_pc_world[:, 2].max()]
         ]
+        self.store.put('/robot/target_bounding_box', bounding_box)
         logger.debug('item bounding box: {}'.format(bounding_box))
-       
+
+        # Construct arguments for planner
         target_item = {
             # 'bbox': [item_position, item_position],
             'bbox': bounding_box,
@@ -44,33 +52,43 @@ class PlanStowGrab(State):
             'drop offset': [0, 0, 0.1],
         } #TODO make sure this info is in db, not stored locally here. why are these values selected?
 
-        self.store.put('/robot/target_bounding_box', bounding_box)
+        # Compute pose
+        # pose = numpy.eye(4)
+        # # normal vector points along Z
+        # pose[:3, 2] = normalize(grasp['orientation'])
+        # pose[:3, 0] = normalize(numpy.cross(rotate(rpy(pi / 2, 0, 0), pose[:3, 2]), pose[:3, 2]))
+        # pose[:3, 1] = normalize(numpy.cross(pose[:3, 2], pose[:3, 0]))
+        # # position is grasp center
+        # pose[:3, 3] = grasp['center']
 
         # compute route
         try:
             if self.store.get('/test/skip_planning', False):
-                motion_plan = [(1,{'robot': self.store.get('/robot/current_config')})]
+                motion_plan = [(1, {'robot': self.store.get('/robot/current_config')})]
                 logger.warn('skipped motion planning for testing')
-
-            else:
+            elif gripper == 'vacuum':
                 logger.info('requesting stow motion plan')
                 self.arguments = {'target_item': target_item}
                 logger.debug('arguments\n{}'.format(self.arguments))
-
                 planner = StowPlanner(self.world, self.store)
                 motion_plan = planner.stow_grab(target_item)
+            else: #mechanical
+                #TODO: develop planner for mechanical gripper
+                raise NotImplementedError('Mechanical gripper planner does not exist')
 
-            if not motion_plan:
+            # Check motion plan
+            if motion_plan is None:
+                self.setOutcome(False)
                 raise RuntimeError('motion plan is empty')
+            else:
+                milestone_map = [m.get_milestone() for m in motion_plan]
+                self.store.put('/robot/waypoints', milestone_map)
+                self.store.put('/robot/timestamp', time())
+                self.setOutcome(True)
+                logger.info('Route generated')
         except Exception:
             self.setOutcome(False)
             logger.exception('Failed to generate motion plan')
-        else:
-            milestone_map = [m.get_milestone() for m in motion_plan]
-            self.store.put('/robot/waypoints', milestone_map)
-            self.setOutcome(True)
-            self.store.put('/robot/timestamp', time())
-            logger.info('Route generated')
 
 if __name__ == '__main__':
     import argparse
