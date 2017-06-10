150e0c728b52e743cee148402aa49465595d68f2
Mark Draelos <mark.draelos@duke.edu>
2017-06-10 18:22:54 -0400

    update SelectItem for stowing

--- src/states/select_item.py
+++ src/states/select_item.py
@@ -10,13 +10,41 @@ class SelectItem(State):
             self.chosenItem = max(self.itemList, key=lambda l: self.itemList[l]['point_value'])
 
         elif self.alg == 'pick':
-            #works under condition that loadOrder has been run. Implement check.
-            self.chosenItem = max(self.itemList, key=lambda l: self.itemList[l]['point_value'])
-            self.store.put('/robot/selected_box', self.store.get('/item/'+self.chosenItem+'/order').replace('order', 'box'))
-            self.store.put('/robot/selected_bin', self.store.get('/item/'+self.chosenItem+'/location'))
+            # #works under condition that loadOrder has been run. Implement check.
+            # self.chosenItem = max(self.itemList, key=lambda l: self.itemList[l]['point_value'])
+            # self.store.put('/robot/selected_box', self.store.get('/item/'+self.chosenItem+'/order').replace('order', 'box'))
+            # self.store.put('/robot/selected_bin', self.store.get('/item/'+self.chosenItem+'/location'))
+
+            self.maxGrasp = None
+
+            # find the best grasp from all the bins
+            for name in ['binA', 'binB', 'binC']:
+                self.url = ['photos', name, 'tcp']
+
+                # get graspability vector from photo-url vacuum_grasps
+                #TODO expand to other grasps
+                self.grasps = self.store.get(self.url + ['vacuum_grasps'])
+
+                maxGrasp = max(self.grasps, key=lambda l: l['score'])
+
+                if self.maxGrasp:
+                    if maxGrasp['score'] < self.maxGrasp['score']:
+                        continue
+
+                self.maxGrasp = maxGrasp
+                print "max grasp is ", maxGrasp
+                self.maxGraspSeg = self.maxGrasp['segment_id'] - 1
+
+                # match greatest graspability segment to the highest likely item ID
+                self.idSeg = self.store.get(self.url + ['detections'])[self.maxGraspSeg]
+                self.chosenItem = max(self.idSeg, key=lambda l: self.idSeg[l])
+
+                #TODO move this outside of if statement
+                self.store.put('/robot/target_grasp', self.maxGrasp)
+                self.store.put('/robot/grasp_location', 'stow_tote')
 
         elif self.alg == 'stow':
-           
+
             self.url = self.store.get('/robot/target_photo_url')
 
             # get graspability vector from photo-url vacuum_grasps
@@ -28,7 +56,6 @@ class SelectItem(State):
             # match greatest graspability segment to the highest likely item ID
             self.idSeg = self.store.get(self.url + ['detections'])[self.maxGraspSeg]
             self.chosenItem = max(self.idSeg, key=lambda l: self.idSeg[l])
-
 
             #TODO move this outside of if statement
             self.store.put('/robot/target_grasp', self.maxGrasp)
