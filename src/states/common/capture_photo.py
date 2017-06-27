c08368659ec5b316e966c1a08d61c5ed4fd1769e
Mark Draelos <mark.draelos@duke.edu>
2017-06-27 18:17:40 -0400

    use new camera server

--- src/states/common/capture_photo.py
+++ src/states/common/capture_photo.py
@@ -2,7 +2,7 @@ import logging
 
 from master.fsm import State
 
-from perception.interface import acquire_images, CameraAcquisitionError, CommandTimeoutError
+from perception.interface import acquire_images_new as acquire_images, CameraAcquisitionError
 
 from . import NoViewingCameraError
 
