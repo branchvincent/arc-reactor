ae6c55271bd05815e7a106b2c409213ef3d9dd6d
branchvincent <branchevincent@gmail.com>
2017-07-15 15:57:51 -0400

    Added script to ensure all joint veloc/accel limits respect rob file

--- src/util/init_robot.py
+++ src/util/init_robot.py
@@ -10,8 +10,31 @@ def init(store=None):
     for ql,qu in zip(*robot.getJointLimits()):
         qi = (ql+qu)/2
         qmid.append(qi)
-        
     store.put('robot/current_config', qmid)
+
+def updateMotionLimits(store=None):
+    def _getValsBelowCeiling(vals, vals_ceiling):
+        vals_new = []
+        for v, v_ceil in zip(vals, vals_ceiling):
+            vmax.append(v if v < v_ceil else v_ceil)
+        return vals_new
+
+    store = store or PensiveClient().default()
+    world = build_world(store)
+    robot = world.robot('tx90l')
+    vmax_abs = robot.getVelocityLimits()
+    amax_abs = robot.getAccelerationLimits()
+
+    # Ensures all v/a limits respect the limits in the rob file
+    for state, values in store.get('planner/states').items():
+        vmax = values['joint_velocity_limits']
+        amax = values['joint_acceleration_limits']
+        vmax_new = _getValsBelowCeiling(vmax, vmax_abs)
+        amax_new = _getValsBelowCeiling(amax, amax_abs)
+        store.put(['planner', 'states', state, 'joint_velocity_limits'], vmax_new)
+        store.put(['planner', 'states', state, 'joint_acceleration_limits'], amax_new)
+
 
 if __name__ == '__main__':
     init()
+    updateMotionLimits()
