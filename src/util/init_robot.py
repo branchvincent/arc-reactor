94c55d8ec92728392d827602b05e0e649b7528f9
Mark Draelos <mark.draelos@duke.edu>
2017-07-23 20:58:14 -0400

    update robot initialization script to set the TCP pose

--- src/util/init_robot.py
+++ src/util/init_robot.py
@@ -1,16 +1,26 @@
 from pensive.client import PensiveClient
-from master.world import build_world
+from master.world import build_world, klampt2numpy
+
+from hardware.control.robotcontroller import EE_LINK_INDEX
 
 def init(store=None):
     store = store or PensiveClient().default()
-    world = build_world(store)
+    world = build_world(store, ignore=['obstacles', 'camera', 'boxes', 'totes', 'shelf'])
     robot = world.robot('tx90l')
     qmid = []
 
     for ql,qu in zip(*robot.getJointLimits()):
         qi = (ql+qu)/2
         qmid.append(qi)
+    robot.setConfig(qmid)
     store.put('robot/current_config', qmid)
+
+    T_tcp = klampt2numpy(robot.link(EE_LINK_INDEX).getTransform())
+    store.put('/robot/tcp_pose', T_tcp)
+    # Update tool camera pose
+    T_cam = store.get('/robot/camera_xform')
+    T = T_tcp.dot(T_cam)
+    store.put('camera/tcp/pose', T)
 
 def updateMotionLimits(store=None):
     def _getValsBelowCeiling(vals, vals_ceiling):
@@ -20,7 +30,7 @@ def updateMotionLimits(store=None):
         return vals_new
 
     store = store or PensiveClient().default()
-    world = build_world(store)
+    world = build_world(store, ignore=['obstacles', 'camera', 'boxes', 'totes', 'shelf'])
     robot = world.robot('tx90l')
     vmax_abs = robot.getVelocityLimits()
     amax_abs = robot.getAccelerationLimits()
