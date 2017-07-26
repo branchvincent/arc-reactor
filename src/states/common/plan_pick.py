373e217bc6c89b90e3c79db8c04b95496cca17db
Mark Draelos <mark.draelos@duke.edu>
2017-07-26 16:55:01 +0900

    try positive and negative normals in pick planner

--- src/states/common/plan_pick.py
+++ src/states/common/plan_pick.py
@@ -26,35 +26,45 @@ class PlanPickBase(State):
         elif gripper not in ['vacuum', 'mechanical']:
             raise RuntimeError('/robot/active_gripper is not unrecognized: "{}"'.format(gripper))
 
-        # Compute item pose
-        T_item = numpy.eye(4)
-        # normal vector points along Z
-        T_item[:3, 2] = normalize(grasp['orientation'])
-        T_item[:3, 0] = normalize(numpy.cross(rotate(rpy(pi / 2, 0, 0), T_item[:3, 2]), T_item[:3, 2]))
-        T_item[:3, 1] = normalize(numpy.cross(T_item[:3, 2], T_item[:3, 0]))
-        # position is grasp center
-        T_item[:3, 3] = grasp['center']
-
         # Plan route
         logger.info('planning pick route for "{}" from "{}"'.format(item, grasp['location']))
 
-        if gripper == 'vacuum':
-            planner = MotionPlanner(store=self.store)
-            if inspect:
-                planner.pickToInspect(T_item)
-            else:
-                planner.pick(T_item)
-        else: #mechanical
-            #TODO: develop planner for mechanical gripper
-            raise NotImplementedError('Mechanical gripper planner does not exist')
+        for sign in [1, -1]:
+            normal = [sign * x for x in grasp['orientation']]
 
-        # Check motion plan
-        motion_plan = self.store.get('/robot/waypoints')
-        if motion_plan is None:
+            # Compute item pose
+            T_item = numpy.eye(4)
+            # normal vector points along Z
+            T_item[:3, 2] = normalize(normal)
+            T_item[:3, 0] = normalize(numpy.cross(rotate(rpy(pi / 2, 0, 0), T_item[:3, 2]), T_item[:3, 2]))
+            T_item[:3, 1] = normalize(numpy.cross(T_item[:3, 2], T_item[:3, 0]))
+            # position is grasp center
+            T_item[:3, 3] = grasp['center']
+
+            try:
+                if gripper == 'vacuum':
+                    planner = MotionPlanner(store=self.store)
+                    if inspect:
+                        planner.pickToInspect(T_item)
+                    else:
+                        planner.pick(T_item)
+                else: #mechanical
+                    #TODO: develop planner for mechanical gripper
+                    raise NotImplementedError('Mechanical gripper planner does not exist')
+
+                # Check motion plan
+                motion_plan = self.store.get('/robot/waypoints')
+                if motion_plan is not None:
+                    logger.info('Route generated')
+                    self.store.put('/status/ppo_done', False)
+                    self.setOutcome(True)
+                    break
+
+            except PlannerFailure:
+                logger.warn('trying other normal')
+                pass
+
+        else:
             self.store.put(['failure', self.getFullName()], 'infeasible')
             self.setOutcome(False)
             logger.error('Failed to generate motion plan')
-        else:
-            logger.info('Route generated')
-            self.store.put('/status/ppo_done', False)
-            self.setOutcome(True)
